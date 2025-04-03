4bcb6c8c7d23cc1035d1f1d20c7111e8a3c342e2
Emre Şahiner <emre@workintech.com.tr>
2025-04-03 22:01:07 +0300

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Link, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import Login from "./pages/Login";
+import Welcome from "./pages/Welcome";
+import Browse from "./pages/Browse";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Switch>
+        <Route exact path="/">
+          <p>Landing Page</p>
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/browse">
+          <Browse />
+        </Route>
+      </Switch>
+      <div className="control_panel">
+        <Link to="/">Ana Sayfa</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/welcome">Welcome</Link>
+        <Link to="/browse">Browse</Link>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
